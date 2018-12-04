0acb6014c65b6355f9c2db0ed2adf721d794bda5
Jungsup Kim <kim8299270@gmail.com>
2018-12-04 21:17:17 +1300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Grid from 'react-css-grid';
+import './App.css';
+import Header from './components/header/header'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/header/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './header.css'
+
+const Header  = (props) => (
+    <header>
+        <div className="header-box">
+          
+            <h1>Here are all recipe for you</h1>
+            <a className="btn btn-left-button" href="#"> I am hungry </a>
+            <a className="btn btn-right-button" href="#">Show me more </a>
+        
+        </div>
+    </header>
+
+);
+
+export default Header;
